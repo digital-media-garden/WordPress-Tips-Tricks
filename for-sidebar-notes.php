61f117100694ca4f075946f98a55acdfed6bea5e
digital-media-garden <99817398+digital-media-garden@users.noreply.github.com>
2023-04-25 20:49:02 -0400

    WP Basics
    Nav and sidebar

---
+++ for-sidebar-notes.php
@@ -0,0 +1,8 @@
+
+<?php
+
+// use these to add stuff to the sidebar
+get_search_form();
+dynamic_sidebar( 'sidebar-1' );
+
+?>
---
+++ wp-nav-basic.php
@@ -0,0 +1,14 @@
+<?php
+
+// WP Nav
+    
+wp_nav_menu(
+    array(
+        'theme_location' => 'main_nav',
+        'container' => '',
+        'menu_class'     => 'main-blog-menu',
+        'depth'          => '1', 
+    )
+);
+
+?>
